4eff59bfa55e98f0ef8a597b30b15b29c4c9ef99
Jatoxo <39669932+Jatoxo@users.noreply.github.com>
2022-04-19 06:11:09 +0200

    Level2

--- Scripts/Level2.gd
+++ Scripts/Level2.gd
@@ -8,8 +8,13 @@ extends BaseLevel
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.spawn = get_node(spawn_node).global_position
+	
+	for child in $Coins.get_children():
+		if child.is_in_group("Coin"):
+			player.totalCoins += 1
 
-
+func allCoins():
+	player.showTextbox(["You have collected all the coins in this level.", "There were only 2.", "You really didn't achive much.", "Just keep running in circles."])
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
